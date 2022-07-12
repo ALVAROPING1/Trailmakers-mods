f2918925202a1972b5adb02a6774b73548b66ff9
ALVAROPING1 <alvaroping1@gmail.com>
2022-07-12 23:39:58 +0200

    added comments to the code

--- Raycaster/main.lua
+++ Raycaster/main.lua
@@ -127,6 +127,7 @@ raycaster = {
 		angle = math.rad(45),
 		fov = math.rad(90)
 	},
+	_wallScallingFactor = 25, -- 75,25 for 80x60, 40,15 for 48x36
 	screen = screen:new({collision = false})
 }
 
@@ -145,6 +146,7 @@ end
 ---
 ---@return nil
 function raycaster:spawn()
+	-- Sets the position of the screen and spawns it
 	---@diagnostic disable-next-line: assign-type-mismatch #The game implements an override to the `+` operator for ModVector3 addition
 	self.screen.position = tm.players.GetPlayerTransform(0).GetPosition() + tm.vector3.Create(0, 0.05, 5)
 	self.screen:spawn()
@@ -155,78 +157,117 @@ end
 ---@return nil
 function raycaster:update()
 	--tm.os.Log("-----------------------------------------------------------------------------------------------------------------------------------------------")
+
+	-- Table with the distance and position returned by each casted ray
 	local rays = {}
 
+	-- Casts a ray for every column on the screen
 	for i = 0, self.screen.sizeH - 1 do
-		table.insert(rays, self:_castRay(self.player.fov * (-1/2 + 1 / (2 * self.screen.sizeH) + i / self.screen.sizeH)))
+		-- Calculates the offset of the ray from the player direction based on the amount of rays and the size of the FOV
+		local offset = self.player.fov * (-1/2 + 1 / (2 * self.screen.sizeH) + i / self.screen.sizeH)
+		table.insert(rays, self:_castRay(offset))
 	end
 
+	-- Draws the screen with the information obtained from the rays
 	self:_drawScreen(rays)
 end
 
+--- Casts a ray with the given offset from the player direction and returns
+--- the distance from the wall it hits and the orientation of that wall
+---
 ---@param angleOffset number
 ---@return {[1]: number, [2]: integer}
 function raycaster:_castRay(angleOffset)
 	local rayAngle = (self.player.angle + angleOffset) % (math.pi * 2)
 	--tm.os.Log("Tracing ray at angle=" .. math.deg(rayAngle))
 
-	local deltaY = 1 - self.player.position.y % 1
+	-- Distance to travel horizontally to go from the player and the next vertical grid line
 	local deltaX = 1 - self.player.position.x % 1
+	-- Distance to travel vertically to go from the player and the next horizontal grid line
+	local deltaY = 1 - self.player.position.y % 1
+	-- Y coordinate of the next intersection with a horizontal grid line
 	local intersectH_Y = math.floor(self.player.position.y) + 1
+	-- X coordinate of the next intersection with a vertical grid line
 	local intersectV_X = math.floor(self.player.position.x) + 1
+
+	-- Distance to travel horizontally to get to the next intersection with a vertical grid line
 	local tileStepX = 1
+	-- Distance to travel vertically to get to the next intersection with a horizontal grid line
 	local tileStepY = 1
 
-
+	-- Vertical direction of the ray
 	local rayDown = rayAngle > math.rad(180)
+	-- Horizontal direction of the ray
 	local rayLeft = rayAngle > math.rad(90) and rayAngle < math.rad(270)
 
+	-- If the ray is going down, update deltaY and tileStepY to go to the previous horizontal
+	-- grid line, and intersectH_Y to be the Y coordinate of the previous horizontal grid line
 	if rayDown then
 		deltaY = deltaY - 1
 		intersectH_Y = intersectH_Y - 1
 		tileStepY = -1
 	end
+	-- If the ray is going left, update deltaX and tileStepX to go to the previous vertical
+	-- grid line, and intersectH_Y to be the Y coordinate of the previous vertical grid line
 	if rayLeft then
 		deltaX = deltaX - 1
 		intersectV_X = intersectV_X - 1
 		tileStepX = -1
 	end
 
+	-- X coordinate of the next intersection with a horizontal grid line
 	local intersectH_X = self.player.position.x + deltaY / math.tan(rayAngle)
+	-- Y coordinate of the next intersection with a vertical grid line
 	local intersectV_Y = self.player.position.y + deltaX * math.tan(rayAngle)
-	local stepX = tileStepY / math.tan(rayAngle)
-	local stepY = tileStepX * math.tan(rayAngle)
-	local hitWall = false
 
+	-- Distance to travel horizontally to get to the next intersection with a horizontal grid line
+	local stepX = tileStepY / math.tan(rayAngle)
+	-- Distance to travel vertically to get to the next intersection with a horizontal grid line
+	local stepY = tileStepX * math.tan(rayAngle)
+
+	-- Values to return
 	local distance
 	local orientation
 
+	-- Loop control variable
+	local hitWall = false
+	-- While the ray hasn't hit a wall, trace it
 	while not hitWall do
+		-- Repeats while the next intersection happens with a vertical grid line
 		while (rayDown and intersectV_Y >= intersectH_Y) or (not rayDown and intersectV_Y <= intersectH_Y) do
 			--tm.os.Log("Intersection vertical grid line at X=" .. intersectV_X .. ", Y=" .. intersectV_Y)
+
+			-- Checks if there is a wall in the tile the ray has entered
 			if self.map[rayLeft and intersectV_X or intersectV_X + 1][math.floor(intersectV_Y) + 1] == 1 then
 				--tm.os.Log("Vertical wall detected at X=" .. intersectV_X .. ", Y=" .. intersectV_Y)
+
+				-- If there is a wall, calculates its distance and orientation, and exits the loop
 				hitWall = true
 				distance = self:_hitWall(intersectV_X, intersectV_Y)
 				orientation = 2
 				break
-			else
-				intersectV_X = intersectV_X + tileStepX
-				intersectV_Y = intersectV_Y + stepY
 			end
+			-- If there isn't a wall, advances the position of the intersection with a vertical grid line to the next position
+			intersectV_X = intersectV_X + tileStepX
+			intersectV_Y = intersectV_Y + stepY
 		end
+		-- Repeats while the next intersection happens with a horizontal grid line
 		while (rayLeft and intersectH_X > intersectV_X) or (not rayLeft and intersectH_X < intersectV_X) do
 			--tm.os.Log("Intersection horizontal grid line at X=" .. intersectH_X .. ", Y=" .. intersectH_Y)
+
+			-- Checks if there is a wall in the tile the ray has entered
 			if self.map[math.floor(intersectH_X) + 1][rayDown and intersectH_Y or intersectH_Y + 1] == 1 then
 				--tm.os.Log("Horizontal wall detected at X=" .. intersectH_X .. ", Y=" .. intersectH_Y)
+
+				-- If there is a wall, calculates its distance and orientation, and exits the loop
 				hitWall = true
 				distance = self:_hitWall(intersectH_X, intersectH_Y)
 				orientation = rayDown and 1 or 3
 				break
-			else
-				intersectH_X = intersectH_X + stepX
-				intersectH_Y = intersectH_Y + tileStepY
 			end
+			-- If there isn't a wall, advances the position of the intersection with a horizontal grid line to the next position
+			intersectH_X = intersectH_X + stepX
+			intersectH_Y = intersectH_Y + tileStepY
 		end
 	end
 
@@ -234,40 +275,57 @@ function raycaster:_castRay(angleOffset)
 	return {distance, orientation}
 end
 
+--- Calculates the distance between the player and the collision point while correcting for the fisheye effect
+---
 ---@param positionX number
 ---@param positionY number
 ---@return number
 function raycaster:_hitWall(positionX, positionY)
+	-- Measures the distance in the direction of the player to prevent the fisheye effect
 	local rawDistance = (positionX - self.player.position.x) * math.cos(self.player.angle) + (positionY - self.player.position.y) * math.sin(self.player.angle)
 	--- Rounds the number to prevent float precision errors
 	---@diagnostic disable-next-line: return-type-mismatch #rawDistance will always be a number
 	return tonumber(string.format("%.10f", rawDistance))
 end
 
+--- Draws the next frame and updates the screen with it
+---
 ---@param rays {[integer]: {[1]: number, [2]: integer}}
 ---@return nil
 function raycaster:_drawScreen(rays)
 	--tm.os.Log("--------- Draw ----------------------------------------------------------------------------------")
+
+	-- Rendered frame
 	local nextFrame = {}
-	local wallScallingFactor = 15 -- 75 for 80x60, 40 for 48x36
+
 	for positionH = 0, self.screen.sizeH - 1 do
 		nextFrame[positionH] = {}
-		local wallHeight = wallScallingFactor / rays[positionH + 1][1]
+
+		-- Height of the wall for that column based on its distance from the player
+		local wallHeight = self._wallScallingFactor / rays[positionH + 1][1]
 		--tm.os.Log("Column=" .. positionH .. ", Height=" .. wallHeight)
 		--tm.os.Log("0<" .. (self.screen.sizeV - wallHeight) / 2 .. "<" .. (self.screen.sizeV - wallHeight) / 2 + wallHeight .. "<infty")
+
+		-- For each pixel in the column, sets its color to that of the floor, sky or
+		-- wall depending on its Y coordinate, with the wall being centered vertically
 		for positionV = 0, self.screen.sizeV - 1 do
 			if positionV < (self.screen.sizeV - wallHeight) / 2 then
+				-- The Y coordinate corresponds to sky
 				nextFrame[positionH][positionV] = 5
 				--tm.os.Log("---Line=" .. positionV .. ", start=" .. (self.screen.sizeV - wallHeight) / 2 .. ", sky")
 			elseif positionV < (self.screen.sizeV - wallHeight) / 2 + wallHeight then
+				-- The Y coordinate corresponds to a wall, use the color given by the ray
 				nextFrame[positionH][positionV] = rays[positionH + 1][2]
 				--tm.os.Log("---Line=" .. positionV .. ", start=" .. (self.screen.sizeV - wallHeight) / 2 .. ", wall")
 			else
+				-- The Y coordinate corresponds to floor
 				nextFrame[positionH][positionV] = 4
 				--tm.os.Log("---Line=" .. positionV .. ", start=" .. (self.screen.sizeV - wallHeight) / 2 .. ", floor")
 			end
 		end
 	end
+
+	-- Pushes the rendered frame to the screen and updates it
 	self.screen:setNextFrameDelta(nextFrame)
 	self.screen:update()
 end
